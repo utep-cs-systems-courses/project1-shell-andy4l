ce6fbfcb385a6faab106fb38439bf5e4fe2bdfa4
Andy <airamos3@miners.utep.edu>
2020-09-13 20:17:50 -0600

    fork implemented but not with correct args

--- osShell.py
+++ osShell.py
@@ -11,9 +11,39 @@ def listFile() :
     print(direct)        
     return   
 
+def getCWD():
+    cdir = os.getcwd()
+    os.write(1, (cdir + "\n").encode())
+    return
 
-os.fork()
-print("Hello World", os.getpid())
+def forkProcess(rc, pid):
+
+    if rc < 0:
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+
+    elif rc == 0:                   # child
+        os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
+        args = ["wc", "p3-exec.py"]
+        for dir in re.split(":", os.environ['PATH']): # try each directory in the path
+            program = "%s/%s" % (dir, args[0])
+            os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
+            try:
+                os.execve(program, args, os.environ) # try to exec program
+            except FileNotFoundError:             # ...expected
+                pass                              # ...fail quietly
+
+        os.write(2, ("Child:    Could not exec %s\n" % args[0]).encode())
+        sys.exit(1)                 # terminate with error
+
+    else:                           # parent (forked ok)
+        os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % (pid, rc)).encode())
+        childPidCode = os.wait()
+        os.write(1, ("Parent: Child %d terminated with exit code %d\n" % childPidCode).encode())
+                 
+
+
+
 while True:
 
     if 'PS1' in os.environ:
@@ -27,17 +57,24 @@ while True:
     # so you know which directory to move to 
     if "cd" in command:
         directory = command.split("cd")[1].strip()
-    try:
-        os.chdir(directory)
-    except FileNotFoundError:
-        os.write(1, ("No such file or directory.\n").encode())
-    continue
+        try:
+            os.chdir(directory)
+        except FileNotFoundError:
+            os.write(1, ("No such file or directory.\n").encode())
+        continue
 
     if command == 'ls':
         listFile()
+        continue
 
     if command == 'quit':
         sys.exit()
-    
+        
+    if "cwd" in command:
+        getCWD()
+        continue
 
-    
+    pid = os.getpid()
+    os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
+    rc = os.fork()
+    forkProcess(rc, pid)
